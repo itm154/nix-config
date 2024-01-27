1084c6fb26e92624516576ed0b4df9c3f5e8f23f
itm154 <ashrulfahmi@gmail.com>
2024-01-27 21:34:15 +0800

    Hyprland: 1

--- home-manager/wm/hyprland/hyprland.nix
+++ home-manager/wm/hyprland/hyprland.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
@@ -12,23 +12,24 @@
         "$mainMod, B, exec, firefox"
 
         # 
-      ]
-      
+      ];
+
       bindm = [
         "$mainMod, mouse:272, movewindow"
         "$mainMod, mouse:273, resizewindow"
       ]
 
-      ++ (
-        # workspaces
-        # binds $mainMod + [shift +] {1..10} to [move to] workspace {1..10}
-        builtins.concatLists (builtins.genList (x:
-          let
-            ws = let c = (x + 1) / 10; in builtins.toString (x + 1 - (c * 10));
-          in [
-            "$mainMod, ${ws}, workspace, ${toString (x + 1)}"
-            "$mainMod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-          ]) 10));
+        ++ (
+          # workspaces
+          # binds $mainMod + [shift +] {1..6} to [move to] workspace {1..10}
+          builtins.concatLists (builtins.genList (x:
+            let
+              ws = let c = (x + 1) / 6;
+              in builtins.toString (x + 1 - (c * 6));
+            in [
+              "$mainMod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mainMod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]) 6));
     };
   };
 }
