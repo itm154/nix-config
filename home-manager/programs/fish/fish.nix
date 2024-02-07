0458e78fd752cdd1c63c316374b9e4110227ca5c
itm154 <ashrulfahmi@gmail.com>
2024-02-07 20:41:30 +0800

    fish and starship config

--- home-manager/programs/fish/fish.nix
+++ home-manager/programs/fish/fish.nix
@@ -4,6 +4,36 @@
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
     '';
+    plugins = [
+      {
+        name = "autopair";
+        src = pkgs.fishPlugins.autopair.src;
+      }
+      {
+        name = "bang-bang";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-bang-bang";
+          rev = "ec991b8";
+          sha256 = "sha256-oPPCtFN2DPuM//c48SXb4TrFRjJtccg0YPXcAo0Lxq0=";
+        };
+      }
+    ];
+    shellAliases = {
+      # Navigation
+      rp = "cd ~/Repository/";
+      dl = "cd ~/Downloads/";
+      docs = "cd ~/Documents/";
+
+      # Colorscheme made it hard to read the texts
+      mixer = "alsamixer --no-color";
+
+      # Better ls
+      ls="eza -al --color=always --group-directories-first";
+      la="eza -a --color=always --group-directories-first";
+      ll="eza -l --color=always --group-directories-first";
+      lt="eza -aT --color=always --group-directories-first";
+    };
   };
 
   programs.starship = {
