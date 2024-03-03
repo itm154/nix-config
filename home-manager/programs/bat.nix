a74b74b9a3c2d37874b04ab47b02aa9c54f90662
itm154 <ashrulfahmi@gmail.com>
2024-03-03 11:03:35 +0800

    Fix formatting

--- home-manager/programs/bat.nix
+++ home-manager/programs/bat.nix
@@ -6,7 +6,10 @@
   programs.bat = {
     enable = true;
     extraPackages = with pkgs.bat-extras; [
-      batdiff batman batgrep batwatch
+      batdiff
+      batman
+      batgrep
+      batwatch
     ];
     config = {
       theme = "Catppuccin-mocha";
@@ -19,7 +22,7 @@
           rev = "ba4d168";
           hash = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
         };
-          file = "Catppuccin-mocha.tmTheme";
+        file = "Catppuccin-mocha.tmTheme";
       };
     };
   };
