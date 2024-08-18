47598a658215aeb444d5ddb9c9ca7356a942f7ba
itm154 <ashrulfahmi@gmail.com>
2024-08-18 12:45:00 +0000

    Enable some test options

--- homes/x86_64-linux/nixos@wsl/default.nix
+++ homes/x86_64-linux/nixos@wsl/default.nix
@@ -11,5 +11,17 @@
 
   cli.gh.enable = true;
 
+  cli.zoxide = {
+    enable = true;
+    fishIntegration = true;
+    aliasCd = true;
+  };
+
+  cli.fish = {
+    enable = true;
+    aliasCommonDir = true;
+    aliasLs = true;
+  };
+
   cli.lazygit.enable = true;
 }
