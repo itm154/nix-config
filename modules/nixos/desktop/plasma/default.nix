65b0fd953c5c1946ece7db10c05a5f6b0833060f
itm154 <ashrulfahmi@gmail.com>
2024-08-24 11:50:54 +0000

    Add plasma module

---
+++ modules/nixos/desktop/plasma/default.nix
@@ -0,0 +1,31 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.custom; let
+  cfg = config.desktop.plasma;
+in {
+  options.desktop.plasma = with types; {
+    enable = mkBoolOpt false "Enable KDE Plasma 6";
+    x11 = mkBoolOpt false "Enable X11 session for Plasma";
+    # INFO: See https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/desktop-managers/plasma6.nix#L136-L149
+    excludePackages = mkOpt (listOf package) [
+      plasma-browser-integration
+      konsole
+      elisa
+    ] "Excluded Plasma packages";
+  };
+
+  config = mkIf cfg.enable {
+    services.xserver = mkIf cfg.x11 {enable = true;};
+    desktop.addons.sddm.enable = true;
+    services.desktopManager.plasma6.enable = true;
+    environment.plasma6.excludePackages = with pkgs.kdePackages;
+      []
+      ++ cfg.excludePackages;
+  };
+}
