3bd7b98f014fe2da4e2370581030eda3a47a576c
itm154 <ashrulfahmi@gmail.com>
2024-08-26 16:14:28 +0000

    Add IME module

---
+++ modules/nixos/services/ime/default.nix
@@ -0,0 +1,32 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.custom; let
+  cfg = config.services.ime;
+in {
+  options.services.ime = with types; {
+    enable = mkBoolOpt false "Enable IME";
+    extraModules = mkOpt (listOf package) [pkgs.fcitx5-mozc] "Extra fcitx5 modules";
+  };
+
+  config = mkIf cfg.enable {
+    i18n.inputMethod = {
+      enabled = "fcitx5";
+      fcitx5.addons = with pkgs; [fcitx5-gtk fcitx5-qt] ++ cfg.extraModules;
+    };
+    services.xserver.desktopManager.runXdgAutostartIfNone = true;
+
+    environment.sessionVariables = {
+      GTK_IM_MODULE = "fcitx";
+      QT_IM_MODULE = "fcitx";
+      SDL_IM_MODULE = "fcitx";
+      GLFW_IM_MODULE = "ibus";
+      XMODIFIERS = "@im=fcitx";
+    };
+  };
+}
