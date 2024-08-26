c588c8206fddbcd8498e16c396f59434ba6d3b44
itm154 <ashrulfahmi@gmail.com>
2024-08-26 16:23:32 +0000

    Add boot module

---
+++ modules/nixos/system/boot/default.nix
@@ -0,0 +1,29 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.custom; let
+  cfg = config.system.boot;
+in {
+  options.system.boot = with types; {
+    enable = mkBoolOpt false "Enable boot";
+    entries = mkOpt str 5 "Amount of entries available";
+  };
+
+  config = mkIf cfg.enable {
+    boot.loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = cfg.entries;
+
+        # NOTE: https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
+        editor = false;
+      };
+      efi.canTouchEfiVariables = true;
+    };
+  };
+}
