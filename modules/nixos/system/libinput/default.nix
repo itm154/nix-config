e469ff7419ead0d8b17b0e3db3767cc955c4e848
itm154 <ashrulfahmi@gmail.com>
2024-08-26 16:14:34 +0000

    Add libinput module

---
+++ modules/nixos/system/libinput/default.nix
@@ -0,0 +1,19 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.custom; let
+  cfg = config.system.libinput;
+in {
+  options.system.libinput = with types; {
+    enable = mkBoolOpt true "Enable system.libinput";
+  };
+
+  config = mkIf cfg.enable {
+    services.libinput.enable = true;
+  };
+}
