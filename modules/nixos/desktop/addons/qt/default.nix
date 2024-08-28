889dbcc8491b0ac294c7f3d5ce726bda765a928b
itm154 <ashrulfahmi@gmail.com>
2024-08-28 23:38:53 +0800

    Add qt module

---
+++ modules/nixos/desktop/addons/qt/default.nix
@@ -0,0 +1,28 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.custom; let
+  cfg = config.desktop.addons.qt;
+in {
+  options.desktop.addons.qt = with types; {
+    enable = mkBoolOpt true "Enable qt theming";
+  };
+
+  config = mkIf cfg.enable {
+    home.extraOptions = {
+      qt = {
+        enable = true;
+        platformTheme = "kvantum";
+        style = {
+          name = "kvantum";
+          catppuccin.enable = true;
+        };
+      };
+    };
+  };
+}
