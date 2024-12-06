72b0ed6460599afe94791b8182d599076d10b480
itm154 <ashrulfahmi@gmail.com>
2024-12-06 21:29:34 +0800

    Update to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My nix flake config based on snowfall-lib";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     snowfall-lib = {
@@ -11,7 +11,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
